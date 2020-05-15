1fb589e13e26bb2b98f6deb881798ad303ae5cf3
Sam-692 <samuelkariuki786@gmail.com>
2020-05-15 16:56:33 +0300

    displaying user account

--- user_test.py
+++ user_test.py
@@ -36,17 +36,17 @@ def test_save_user(self):
         self.assertEqual(len(User.user_list),1)
 
 # setup and class creation up here
-def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
-            User.user_list = []
+# def tearDown(self):
+#             '''
+#             tearDown method that does clean up after each test case has run.
+#             '''
+#             User.user_list = []
 
-def test_display_account(self):
-        """
-        method that return a list of acounts
-        """
-        self.assertEqual(User.display_account(),User.user_list)
+# def test_display_account(self):
+#         """
+#         method that return a list of acounts
+#         """
+#         self.assertEqual(User.display_account(),User.user_list)
 # other test cases here
 def test_save_multiple_user(self):
             '''
@@ -58,16 +58,16 @@ def test_save_multiple_user(self):
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
 
-# def test_delete_user(self):
-#             '''
-#             test_delete_user to test if we can remove a user from our user list
-#             '''
-#             self.new_user.save_user()
-#             test_user = User("Test","user","mtoto","785uemd,") # new user
-#             test_user.save_user()
+def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user","mtoto","785uemd,") # new user
+            test_user.save_user()
 
-#             self.new_user.delete_user()# Deleting a user object
-#             self.assertEqual(len(User.user_list),1)
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)
 
 def delete_user(self):
 
